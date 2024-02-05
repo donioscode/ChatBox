14d4c5097e57e8390018e6f73c2d55005a23701b
donioscode <doniyornormuxammedov@gmail.com>
2024-02-05 20:44:48 +0500

    Initial Commit

---
+++ ChatBox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatBox
+//
+//  Created by doniyor normuxammedov on 05/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
